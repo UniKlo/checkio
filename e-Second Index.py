b5b07c04108bde5b08bdfff048e6750c17e2a4c2
UniKlo <33693941+UniKlo@users.noreply.github.com>
2017-11-29 18:43:57 -0500

    Add files via upload

---
+++ e-Second Index.py
@@ -0,0 +1,20 @@
+def second_index(x,y):
+    a=x.find(y)
+    b=x.find(y,a+1)
+    if b==-1 :
+        b=None
+
+    return b
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
