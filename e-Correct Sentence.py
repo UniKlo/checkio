e04797161b71490d60ff82c7824aa615b91a1043
UniKlo <33693941+UniKlo@users.noreply.github.com>
2017-11-29 19:39:49 -0500

    Add files via upload

---
+++ e-Correct Sentence.py
@@ -0,0 +1,17 @@
+def correct_sentence(text):
+
+    if text[-1] != "." :
+        text = text + "."
+    return text[0].upper() + text[1:]
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
