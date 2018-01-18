ba84dd7e281e062e7f081af82662a760a782bc5e
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-01-17 23:29:53 -0500

    Add files via upload

---
+++ checkline.py
@@ -0,0 +1,20 @@
+def check_line(line):
+    l=len(line)
+    for n in range (0,l-1):
+        if line[n]!=line[n+1]:
+            continue
+        else:
+            return False
+    return True
+        
+
+   
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_line(["X", "Z", "X"]) == True
+    assert check_line(["X", "Z", "X", "X"]) == False
+    assert check_line(["X", "Z"]) == True
+    assert check_line(["Z", "X"]) == True
+    assert check_line(["Z", "X", "Z", "X", "Z", "Z", "X"]) == False
+
+    print("Code's finished? Earn rewards by clicking 'Check' to review your tests!")
