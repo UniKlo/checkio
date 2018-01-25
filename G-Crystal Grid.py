0244ebddcbf8df87cdb351b6c115b6d1ce4f34c5
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-01-24 22:57:04 -0500

    Add files via upload

---
+++ G-Crystal Grid.py
@@ -0,0 +1,31 @@
+def check_grid(grid):
+    for row in grid:
+        for i in range (len(row)-1):
+            if row[i]==row[i+1]:
+                return False
+    rotated_grid = list(zip(*grid))
+    for row2 in rotated_grid:
+        for i in range (len(row2)-1):
+            if row2[i]==row2[i+1]:
+                return False
+    return True
+
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_grid([["X", "Z"], ["Z", "X"]]), "2x2 Good"
+    assert check_grid([["X", "Z", "X"],
+                       ["Z", "X", "Z"],
+                       ["X", "Z", "X"]]), "3x3 Good"
+    assert check_grid([["X", "Z", "X", "Z"],
+                       ["Z", "X", "Z", "X"]]), "2x4 Good"
+    assert not check_grid([["X", "X"],
+                           ["X", "X"]]), "2x2 Bad"
+    assert not check_grid([["X", "Z", "X"],
+                           ["Z", "Z", "Z"],
+                           ["X", "Z", "X"]]), "3x3 Bad"
+    assert not check_grid([["X", "Z", "X", "Z"],
+                           ["X", "Z", "X", "Z"]]), "2x4 Bad"
+
+    print("Use 'Check' to earn sweet rewards!")
