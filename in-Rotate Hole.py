1539f77b7b52562f298aba8f91f31a1124ed33a2
UniKlo <33693941+UniKlo@users.noreply.github.com>
2018-02-16 15:50:38 -0500

    Update in-Rotate Hole.py

--- in-Rotate Hole.py
+++ in-Rotate Hole.py
@@ -1,28 +1,26 @@
 import numpy as np
+np.set_printoptions(threshold=np.nan)
 def rotate(state, pipe_numbers):
-    print ('len of the state: ',len(state))
+    #print ('len of the state: ',len(state))
     state=np.array(state)
     answer=[]
     turns=0
-    while turns < (len(state)-1):
-        '''
-        for position in range (len(state)):
-            print ('position: ', position)
-            for n in pipe_numbers:
-                #print ('state[n]: ', state[n])
-                if state[n]!=1:
-                    answer=[]
-                    break
-                else:
-                    answer.append(position)
-            answer.append(position)
-            '''
-        turns +=1
-        print (turns)
-        print (np.roll(state,turns))
-        #print (state)
-    return answer
+    while turns < (len(state)):
 
+        test=[]
+        for n in pipe_numbers:
+            #print ('state[n]: ', state[n])
+            test.append(state[n])
+        #print ('test: ',test)
+        if 0 in test:
+            pass
+        else:
+            answer.append(turns)
+            
+        state=np.roll(state,1)
+        turns +=1
+        
+    return answer
 
 print(rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]))
 '''
